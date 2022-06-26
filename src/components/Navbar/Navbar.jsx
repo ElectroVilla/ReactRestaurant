ced92b1454f2343a85388d311a2fb9e4687291ae
Mina <electronixboy@gmail.com>
2022-06-26 13:56:38 +0200

    Final

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
 import { images } from '../../constants';
 import './Navbar.css';
 
-const Navbar = () => (
+const Navbar = () => {
+  const [toggleMwnu, setToggleMwnu] = useState(false);
+  return (
   <nav className="app__navbar">
     <div className="app__navbar-logo">
       <img src={images.gericht} alt="app logo" />
@@ -22,16 +24,24 @@ const Navbar = () => (
       <a href="/" className="p__opensans">Book Table</a>
     </div>
     <div className="app__navbar-smallscreen">
-      <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => { }} />
-      <ul className="app__navbar-smallscreen-links">
-        <li className="p__opensans"><a href="#home">Home</a></li>
-        <li className="p__opensans"><a href="#about">About</a></li>
-        <li className="p__opensans"><a href="#menu">Menu</a></li>
-        <li className="p__opensans"><a href="#awards">Awards</a></li>
-        <li className="p__opensans"><a href="#contact">Contact</a></li>
-      </ul>
+      <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => setToggleMwnu(true)} />
+
+      {toggleMwnu && (
+      <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+        <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMwnu(false)} />
+        <ul className="app__navbar-smallscreen-links">
+          <li className="p__opensans"><a href="#home">Home</a></li>
+          <li className="p__opensans"><a href="#about">About</a></li>
+          <li className="p__opensans"><a href="#menu">Menu</a></li>
+          <li className="p__opensans"><a href="#awards">Awards</a></li>
+          <li className="p__opensans"><a href="#contact">Contact</a></li>
+        </ul>
+      </div>
+      )}
+
     </div>
   </nav>
-);
+  )
+};
 
 export default Navbar;
--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
-
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import { meal } from '../../constants';
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
+const Intro = () => {
+  const [playVideo, setPlayVideo] = React.useState(false);
+  const vidRef = React.useRef();
+  const handleVideo = () => {
+    setPlayVideo(!playVideo);
+    if (playVideo) {
+      vidRef.current.pause();
+      document.getElementById('mina').classList.replace("app__video-overlay_off", "app__video-overlay");
+    }else {
+      vidRef.current.play();
+      document.getElementById('mina').classList.replace("app__video-overlay", "app__video-overlay_off");
+    }
+  }
+  return (
+  <div className="app__video">
+    <video
+      ref={vidRef}
+      src={meal}
+      type="video/mp4"
+      loop
+      controls={false}
+      muted
+    />
+    <div className="app__video-overlay flex__center" id='mina'>
+      <div className="app__video-overlay_circle flex__center"
+        onClick={handleVideo}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+      </div>
+    </div>
   </div>
-);
+  );
+};
 
 export default Intro;
